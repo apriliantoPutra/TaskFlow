b5080fff0f91d35ef90591f8d4321c475a0a0e21
apriliantoPutra <apriliantoputra696@gmail.com>
2025-11-19 18:46:00 +0700

    Kirim backend

--- back-end/app.js
+++ back-end/app.js
@@ -8,6 +8,8 @@ require("./src/cron/taskScheduler");
 const app= express();
 app.use(cors());
 app.use(express.json());
+const PORT = process.env.PORT || 5000;
+
 
 app.get('/', (req, res)=> {
     res.json({ message: "Selamat Datang di API aplikasi TaskFlow" });
@@ -16,7 +18,7 @@ app.get('/', (req, res)=> {
 app.use('/api', authRoutes);
 app.use('/api/tasks', taskRoutes);
 
-app.listen(process.env.PORT, ()=> {
-    console.log(`Server running on http://localhost:${process.env.PORT}`);
-})
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
 
