89924dfcf673873ba7c8610711eba8c8bd950df3
apriliantoPutra <apriliantoputra696@gmail.com>
2025-11-19 19:35:21 +0700

    Perbaikan Backend

--- back-end/app.js
+++ back-end/app.js
@@ -1,19 +1,27 @@
-const express= require('express');
+const express = require('express');
 require("dotenv").config();
-const cors= require('cors');
-const authRoutes= require('./src/routes/authRoutes');
-const taskRoutes= require('./src/routes/taskRoutes');
+const cors = require('cors');
+const authRoutes = require('./src/routes/authRoutes');
+const taskRoutes = require('./src/routes/taskRoutes');
 require("./src/cron/taskScheduler");
 
-const app= express();
-app.use(cors());
+const app = express();
+
+// Konfigurasi CORS hanya sekali
+const corsOptions = {
+  origin: [
+    'https://task-flow-rho-ten.vercel.app', 
+  ],
+  credentials: true, 
+};
+app.use(cors(corsOptions));
+
 app.use(express.json());
 const PORT = process.env.PORT || 5000;
 
-
-app.get('/', (req, res)=> {
-    res.json({ message: "Selamat Datang di API aplikasi TaskFlow" });
-})
+app.get('/', (req, res) => {
+  res.json({ message: "Selamat Datang di API aplikasi TaskFlow" });
+});
 
 app.use('/api', authRoutes);
 app.use('/api/tasks', taskRoutes);
@@ -21,4 +29,3 @@ app.use('/api/tasks', taskRoutes);
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
-
--- back-end/src/utils/emailService.js
+++ back-end/src/utils/emailService.js
@@ -8,6 +8,7 @@ const transporter= nodemailer.createTransport({
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
     },
+    secure: true,
 })
 
 async function sendVerificationEmail(to, token) {
